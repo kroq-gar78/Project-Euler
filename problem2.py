7fc9801f9ae86f8ca469fa421e0a4f7e93a3ff1c
Aditya Vaidya <kroq.gar78@gmail.com>
2011-12-21 12:00:44 -0600

    Project Euler problem 2: correct solution. I didn't look

--- problem2.py
+++ problem2.py
@@ -8,5 +8,7 @@ if __name__ == "__main__":
 		fib1=fib1+fib2
 		fib2=fib1+fib2
 		if(fib1&1==0): total=total+fib1
-		if(fib1&1==0): total=total+fib2
+		if(fib2&1==0): total=total+fib2
+	if(fib1&1==0): total=total-fib1
+	if(fib2&1==0): total=total-fib2
 	print total
