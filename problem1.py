ebf162c6ef29842576a37c0d437376b6b615b15b
Aditya Vaidya <kroq.gar78@gmail.com>
2011-12-21 01:54:59 -0600

    Project Euler Problem 1: I looked. Sad.

--- problem1.py
+++ problem1.py
@@ -8,20 +8,8 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 if __name__ == "__main__":
-	a=3
-	asum=a
-	b=5
-	bsum=b
-	while a<1000:
-		a=a+3
-		print a
-		asum=asum+a
-	asum=asum-a
-	while b<1000:
-		b=b+5
-		print b
-		bsum=bsum+b
-	bsum=bsum-b
-	total=asum+bsum
-	print total
-	
+	answer = 0
+	for i in xrange(1,1000):
+		if not i%3: answer=answer+i
+		elif not i%5: answer=answer+i
+	print answer
