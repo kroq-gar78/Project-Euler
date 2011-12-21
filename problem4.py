db27454596b7136c3d7269ae2a1fc4070d3db4bc
Aditya Vaidya <kroq.gar78@gmail.com>
2011-12-21 12:45:06 -0600

    Project Euler problem 4: slight optimization.

--- problem4.py
+++ problem4.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+#
+# Project Euler
+#
+
+# Problem 4
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
 def isPalindrome(n): # 6 digit palindromes only
 	string = str(n)
 	return (string[0]==string[5] and string[1]==string[4] and string[2]==string[3])
@@ -10,6 +17,5 @@ if __name__ == "__main__":
 	for i in xrange(100000,999999):
 		if isPalindrome(i):
 			for j in xrange(100,999):
-				if i%j==0:
-					if(len(str(i/j))==3): candidates.append(i)
+				if i%j==0: candidates.append(i)
 	print candidates[len(candidates)-1]
