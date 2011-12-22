ebe2a8787376758f938ccea5f160f4f829b8f1f3
Aditya Vaidya <kroq.gar78@gmail.com>
2011-12-21 18:13:57 -0600

    Project Euler problem 80: incorrect, will come back.

--- problem80.py
+++ problem80.py
@@ -6,28 +6,31 @@
 
 # Problem 80
 # For the first one hundred natural numbers, find the total of the
-# digital sums of the first one hundred decimal digits for all the irrational square roots.
+# digital sums of the first one hundred decimal digits for all the
+# irrational square roots.
 
 import decimal
 
-def digiroot(n):
+def digisum(n):
 	#print n
 	n=str(n)
 	if '.' in n:
-		n = n.replace('.',"")
+		#n = n.replace('.',"")
+		n=str(n[n.index('.')+1:])
+		print n
 		#print n
+	else: return 0
 	ret=0
 	for i in n:
 		#print i
 		ret=ret+int(i)
 		#print ret
-	if(len(str(ret))>1): ret = digiroot(ret)
 	return ret
 	
 if __name__ == "__main__":
 	decimal.getcontext().prec = 100
 	total=0
-	print digiroot(decimal.Decimal(2).sqrt())
+	print digisum(decimal.Decimal(2).sqrt())
 	for i in xrange(1,100):
-		total=total+digiroot(decimal.Decimal(i).sqrt())
+		total=total+digisum(decimal.Decimal(i).sqrt())
 	print total
