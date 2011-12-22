8e120d7835462e5c058d7ff7cafb18b46f2dd703
Aditya Vaidya <kroq.gar78@gmail.com>
2011-12-21 19:36:20 -0600

    Project Euler problem 9: optimized.

--- problem9.py
+++ problem9.py
@@ -10,11 +10,6 @@
 
 if __name__ == "__main__":
 	for a in xrange(3,500,2):
-		answerfound=False
 		for b in xrange(3,500):
-			if a+b>1000: break
-			c=(a**2+b**2)**(0.5)
-			if(int(c)!=c): continue # check if answer has decimals
-			perim=a+b+c
-			if perim>1000: continue
-			if perim==1000: print a*b*c; exit()
+			c=1000-a-b
+			if a**2+b**2==c**2: print int(a*b*c); exit()
