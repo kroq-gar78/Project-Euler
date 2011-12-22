8c01d2847e54dd7e77e45cb2b0bc64f3c06f8ab2
Aditya Vaidya <kroq.gar78@gmail.com>
2011-12-21 19:31:21 -0600

    Project Euler problem 9: removed unnecessary steps.

--- problem9.py
+++ problem9.py
@@ -9,15 +9,12 @@
 # Find the product abc.
 
 if __name__ == "__main__":
-	sides = [0,0,0]
 	for a in xrange(3,500,2):
 		answerfound=False
 		for b in xrange(3,500):
-			#if a+b>1000: break
+			if a+b>1000: break
 			c=(a**2+b**2)**(0.5)
 			if(int(c)!=c): continue # check if answer has decimals
 			perim=a+b+c
 			if perim>1000: continue
-			if perim==1000: sides=[a,b,c]; break; answerfound=True
-		if answerfound: break
-	print sides[0]*sides[1]*sides[2]
+			if perim==1000: print a*b*c; exit()
