3f8e16d1dd1a84d23bf7b222be336ff731437975
Aditya Vaidya <kroq.gar78@gmail.com>
2011-12-24 14:49:51 -0600

    Project Euler problem 193: still working on it, not solved.

--- problem193.py
+++ problem193.py
@@ -4,14 +4,12 @@
 # Project Euler
 #
 
-# Problem 14
+# Problem 193
 # How many squarefree numbers are there below 250?
 #
-# factorize() function (integrated into main) taken from one of my older programs
+# factorize() function (integrated into sqfree()) taken from one of my older programs
 
-def sqfree(n,primes=[2]):
-	sqfree=True
-	factors = {}
+def is_sqfree(n,primes=[2]):
 	factor = 0
 	for i in primes:
 		if(n%i==0):
@@ -29,22 +27,21 @@ def sqfree(n,primes=[2]):
 					factor=i
 					break
 				if(not i in primes): primes.append(i)
-		if factor==0: return {n:1}
+		if factor==0: return True
 			
 	exp=0
 	while(n%factor==0):
-		exp=exp+1
-		n=n/factor
-	factors[factor]=exp
-	if(n!=factor and n!=1):
-		results=factorize(n)
-		for i in results.keys():
-			factors[i]=results[i]
-	return factors
+		if exp+1>=2: return False
+		exp+=1
+		n/=factor
+		if n==factor: return False
+	if exp>=2: return False
+	if(n!=1):
+		if not is_sqfree(n): return False
+	return True
 
 if __name__ == "__main__":
 	num_notsqfree=0
-	primes=[2]
 	for n in xrange(1,1<<50):
-		
+		if is_sqfree(n): num_notsqfree+=1
 	print num_notsqfree
