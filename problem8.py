adba7d438e9abddd37b3232413cb7d914b955adb
Aditya Vaidya <kroq.gar78@gmail.com>
2012-01-13 18:07:03 -0600

    Project Euler problem 8: added old improvement.

--- problem8.py
+++ problem8.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
-# 
 # Project Euler
-#
+# http://projecteuler.net/problem=8
 
 # Problem 8
-# Find the greatest product of five consecutive digits in the
-# 1000-digit number (in data file "problem8_data.txt")
+# Find the greatest product of five consecutive digits in the 1000-digit
+# number (in file "problem8_data.txt").
 
 if __name__ == "__main__":
-	number=open("problem8_data.txt",'r').readline()
-	digitproducts=[]
-	for i in reversed(range(5)):
-		if str(i*(i+1)*(i+2)*(i+3)*(i+4)) in number:
-			print i
-			print i*(i+1)*(i+2)*(i+3)*(i+4)
-			exit()
-		
+	number=str(int(open("problem8_data.txt",'r').read())) # str to int to str removes any extra formatting
+	greatest=0
+	for i in xrange(len(number)-5):
+		product=int(number[i])*int(number[i+1])*int(number[i+2])*int(number[i+3])*int(number[i+4])
+		print product
+		print int(number[i]),int(number[i+1]),int(number[i+2])
+		if product>=greatest: # huh???? why syntax error??? FOUND YA!
+			greatest=product
+	print greatest # silly me
