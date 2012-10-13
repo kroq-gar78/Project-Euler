ee7e37e551b37cdbe8bb02680f00142cc9b69a7c
Aditya Vaidya <kroq.gar78@gmail.com>
2012-10-13 12:12:01 -0500

    Problem 73: cleanup

--- problem73.py
+++ problem73.py
@@ -13,16 +13,13 @@ def gcd(a,b):
 	return a
 
 if(__name__ == '__main__'):
-	#fractions = []
 	answer = 0
 	for i in xrange(2,12001):
 		for j in xrange(1,i,(2 if i&1==0 else 1)): # skip evens if 'i' is even
 			jdivi = float(j)/i
-			#print jdivi
 			if(jdivi > 1.0/3):
 				if(jdivi < 1.0/2):
 					gcdij = gcd(i,j)
 					if gcdij == 1:
 						answer += 1
-						#fractions.append([i/gcdij,j/gcdij])
 	print answer
