ec4aa29b084b7fb7aae806200419da432bd7a7a7
Aditya Vaidya <kroq.gar78@gmail.com>
2014-11-21 22:05:49 -0600

    Problem 24: brute force + Python = an easy time

--- problem24.py
+++ problem24.py
@@ -7,13 +7,11 @@
 # What is the millionth lexicographic permutation of the digits
 # 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9 after being numerically ordered?
 
+# SOLVED: iterate through all perms, sort, and get the millionth one.
+# Python makes this one way too easy.
+
 import itertools
 
 if __name__ == "__main__":
-	digits=[0,1,2,3,4,5,6,7,8,9]					
-	perms=[]
-	for i in xrange(len(digits)):
-		for subset in itertools.permutations(digits):
-			perms.append(subset)
-	
-	print perms[1000000]
+	digits=[0,1,2,3,4,5,6,7,8,9]
+	print ''.join(list(sorted(itertools.permutations(map(lambda x: str(x),digits)))[999999])) # ugly and cool one-liner
